19ef0db33b3981f0d77e76de690a8578f0b7b9a4
BobDev <BobDev@10.72.103.18>
2015-01-31 12:16:57 -0500

    DriveTrain Fully functional

--- src/org/usfirst/frc319/commands/DriveTrainArcadeDrive.java
+++ src/org/usfirst/frc319/commands/DriveTrainArcadeDrive.java
@@ -35,6 +35,7 @@ public class  DriveTrainArcadeDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.driveTrain.arcadeDrive();
     }
 
     // Make this return true when this Command no longer needs to run execute()
