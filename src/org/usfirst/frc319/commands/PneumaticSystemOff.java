93b421772b4d54baf3053e872ee4c37015f77ea0
BobDev <BobDev@10.72.103.18>
2015-02-04 15:53:29 -0500

    All elevator pnuematics are in and working... drive train still works
    ON THE FULLY FUNCTIONAL ROBOT
    
    Signed-off-by: BobDev <BobDev@10.72.103.18>

--- src/org/usfirst/frc319/commands/PneumaticSystemOff.java
+++ src/org/usfirst/frc319/commands/PneumaticSystemOff.java
@@ -35,6 +35,7 @@ public class  PneumaticSystemOff extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.pnuematicSystem.compressorOff();
     }
 
     // Make this return true when this Command no longer needs to run execute()
